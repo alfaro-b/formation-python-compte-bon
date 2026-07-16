a2634c1197304220d870d638c715ddb65b790216
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-16 13:12:11 +0200

    ajout fonction pour tirer au sort un nombre entier entre 101 et 999

--- compte_bon.py
+++ compte_bon.py
@@ -1,3 +1,6 @@
+import random
+
+
 def create_plates_stock():
     plates_stock = []
     for i in range(1, 11):
@@ -8,6 +11,13 @@ def create_plates_stock():
     return plates_stock
 
 
+def choose_target_number():
+    number = random.randint(101, 1000)
+    return number
+
+
 initial_plates_stock = create_plates_stock()
+target_number = choose_target_number()
 
 print(initial_plates_stock)
+print(target_number)
